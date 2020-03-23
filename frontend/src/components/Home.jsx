626aa16a06bf602c4c4003b8a73edf6e7ad14bd5
Alex Markowitz <Alex@alexs-mbp-3.lan>
2020-03-23 00:00:10 -0400

    added grayscale togglin

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Photo from "./Photo";
+
 import axios from "axios";
+import Switch from "react-switch";
 
 import { API_URL } from "../constants";
 
 export default function Home() {
   const [data, setData] = useState({ photos: [] });
+  const [color, setColor] = useState(true);
 
   function fetchPhotos() {
     axios.get(API_URL).then(res => setData({ photos: res.data }));
@@ -15,14 +18,19 @@ export default function Home() {
     fetchPhotos();
   }, []);
 
+  function toggleColor() {
+    setColor(!color);
+  }
+
   function getPhotos() {
+    const grayscaleURL = color ? "" : "?grayscale";
     return (
       <div>
         {data.photos.map(photo => {
           return (
             <Photo
               key={photo.photo_id + photo.height + photo.width}
-              url={photo.url}
+              url={photo.url + grayscaleURL}
               height={photo.height}
               width={photo.width}
               photoId={photo.photo_id}
@@ -33,5 +41,12 @@ export default function Home() {
     );
   }
 
-  return <div>{getPhotos()}</div>;
+  return (
+    <div>
+      <div>
+        <Switch onChange={toggleColor} checked={color} />
+      </div>
+      {getPhotos()}
+    </div>
+  );
 }
