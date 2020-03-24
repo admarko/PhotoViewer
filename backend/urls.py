d5de66d5b73a29a8755fb867bd8026527a65c873
Alex Markowitz <Alex@alexs-mbp-3.lan>
2020-03-24 00:01:31 -0400

    Added pagination

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+from .views import PhotosList
 
 urlpatterns = [
-    path("api/photos", views.photos_list, name="photos-list"),
+    # path("api/photos", views.photos_list, name="photos-list"), # depricated
+    path("api/photos", PhotosList.as_view()),
     path("api/photos/<int:photo_id>", views.photo, name="photo-detail"),
 ]
